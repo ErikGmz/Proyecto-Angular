aa308f00e23b6904933dc71581973ffa3f2554ab
ErikGmz <erikgomez198755@outlook.com>
2022-04-30 03:25:21 -0500

    Incorporación de formulario para registrar fanáticos.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
+import {formatDate} from '@angular/common';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  constructor() { }
+  nombre!:string;
+  sexo!:string;
+  correo!:string;
+  fechaNacimiento!:string;
 
-  ngOnInit(): void {}
+  @ViewChild("formulario") formulario!: ElementRef;
+
+  constructor() {
+    this.borrarDatos();
+  }
+
+  ngOnInit():void {}
+
+  getFechaActual():string {
+    return formatDate(Date.now() - 315576000000,'yyyy-MM-dd','en-US');
+  }
+
+  borrarDatos():void {
+    this.nombre = "";
+    this.sexo = "";
+    this.correo = "";
+    this.fechaNacimiento = "";
+  }
+
+  enviarDatos($event:any):void {
+    if(this.nombre && this.sexo && this.correo && this.fechaNacimiento) {
+      let datosUsuario = {
+        nombre: this.nombre,
+        sexo: this.sexo,
+        correo: this.correo,
+        fechaNacimiento: this.fechaNacimiento
+      }
+
+      let datosFans = localStorage.getItem("datosFans");
+      let arreglo = new Array();
+      if(datosFans) {
+        arreglo = JSON.parse(datosFans);
+      }
+      arreglo.push(datosUsuario);
+      localStorage.setItem("datosFans", JSON.stringify(arreglo));
+      alert("El fanático " + this.nombre + " fue exitosamente registrado.");
+      this.borrarDatos();
+    }
+    this.formulario.nativeElement.reportValidity();
+  }
 }
